791653c4a33cd5af0e9a670ce3eff2f752d6b2c1
Phan Giap <giapps@gmail.com>
2019-04-09 00:06:50 +0700

    fix speed issue

--- vendor/custom/custom.js
+++ vendor/custom/custom.js
@@ -134,7 +134,7 @@ particlesJS("particles-js", {
       },
       "move": {
         "enable": true,
-        "speed": 5,
+        "speed": 20,
         "direction": "none",
         "random": false,
         "straight": false,
