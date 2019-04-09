ee48270a5fcb5e5dee10ffdd0713928c7efb13e3
Phan Giap <giapps@gmail.com>
2019-04-09 22:55:25 +0700

    animation

--- js/anime.js
+++ js/anime.js
@@ -1,29 +1,63 @@
 var randomMovement = function() {
-    return anime.random(-20, 20) + 'rem'
+    return anime.random(0, 200) + 'rem'
 };
 
 var randomSpeed = function() {
   return anime.random(1000, 5000) + 'rem'  
 };
 
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
+}
+
+function randomPostionX() {
+    return Math.floor(Math.random()*document.body.offsetHeight);
+}
+
+function randomPostionY() {
+    return Math.floor(Math.random()*document.body.offsetHeight);
+}
+
 $(document).ready(function(){
 	animation();
 });
 
+function ramdomSet(){
+    var bodyWidth = document.body.clientWidth
+    var bodyHeight = document.body.clientHeight;
+    $('.bubble').each( function(index){
+        $(this).css({
+            left: Math.random() * (bodyWidth - $(this).width()),
+            top: Math.random() * (bodyHeight - $(this).height()),
+            position: 'absolute',
+        });
+    });
+}
+
 function animation() {
-console.log("animation");
+
+    ramdomSet();
+    
     var timelineParameters = anime.timeline({
         loop: true
     });
 
     timelineParameters
-    .add({
+    /*.add({
         //targets: ['#bubble1', '#bubble2', '#bubble3', '#bubble4'],
         targets: ['#bubble1'],
         translateX: 0,
         translateY: 0,
-        duration: 100
-        })
+        duration: 0,
+
+        /*left: randomPostionX() + 'px',
+        /* top: randomPostionY() + 'px',
+        position: reletive,
+        })*/
     .add({
         targets: '#bubble1',
         translateX: [ { value: randomMovement  }, { value: randomMovement }, { value: randomMovement } ],
@@ -61,3 +95,4 @@ console.log("animation");
     })*/;
     timelineParameters.complete = function() {animation();};
 }
+
